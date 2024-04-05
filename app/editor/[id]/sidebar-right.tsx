e3004e3c5c868544bbfb0c624a9a3e1b90b3b3b3
Jeeho Ahn <io@jcde.xyz>
2024-04-05 09:46:12 +0900

    s

--- app/editor/[id]/sidebar-right.tsx
+++ app/editor/[id]/sidebar-right.tsx
@@ -1,9 +1,17 @@
 import { ToC } from "./toc";
 import React from "react";
 import TagMaker from "@/components/tag/tag-maker";
+import { TableOfContentData } from "@tiptap-pro/extension-table-of-contents";
+import { Note } from "@prisma/client";
 
 const MemorizedToC = React.memo(ToC);
-export const RightSidebar = ({ items, n }) => {
+export const RightSidebar = ({
+  items,
+  n,
+}: {
+  items: TableOfContentData;
+  n: Note;
+}) => {
   return (
     <div className="hidden xl:block absolute right-0 inset-y-4 w-80 p-4 text-sm">
       <MemorizedToC items={items} />
